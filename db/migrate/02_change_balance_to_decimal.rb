08fed151be7075fe90c4e03f74f1a9582ef48721
Aaron Berns <anberns@gmail.com>
2019-01-30 13:41:57 +0000

    Automatically backed up by Learn

--- db/migrate/02_change_balance_to_decimal.rb
+++ db/migrate/02_change_balance_to_decimal.rb
@@ -1,4 +1,4 @@
-class ChangeDatatypeForBirthdate < ActiveRecord::Migration[4.2]
+class ChangeBalanceToDecimal < ActiveRecord::Migration[4.2]
   def change
     change_column :users, :balance, :deciman
   end
