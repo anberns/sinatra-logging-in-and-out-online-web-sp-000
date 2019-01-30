5dc4fa1f8efaaf6185d6a9953f0c9203e996e7d1
Aaron Berns <anberns@gmail.com>
2019-01-30 13:42:01 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 2) do
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
-    t.integer "balance"
-  end
+# Could not dump table "users" because of following StandardError
+#   Unknown type 'deciman' for column 'balance'
 
 end
